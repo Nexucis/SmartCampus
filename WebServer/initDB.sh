bf892d1e4cbbda46830babf7b114a09b5b578696
Augustin <nexucis@gmail.com>
2015-02-26 14:57:10 +0100

    12929-2 fin du script bash d'init de la bdd

--- WebServer/initDB.sh
+++ WebServer/initDB.sh
@@ -2,9 +2,9 @@
 
  #import sensor
 mongoimport --db test --collection sensors_data < dbJSON/sensor.json
-
  #import item 
-
+mongoimport --db test --collection item < dbJSON/item.json
  #import entity
-
+mongoimport --db test --collection entity < dbJSON/entity.json
  #import admin
+mongoimport --db test --collection administrator < dbJSON/admin.json
