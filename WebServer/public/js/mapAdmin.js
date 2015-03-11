74965e1c1d0225a40b454add9ac548a8fbb4939b
Rodolphe <rfreby@gmail.com>
2015-03-11 14:40:46 +0100

    14504-5 Fin de l'ajout de la map pour la sélection de capteurs

--- WebServer/public/js/mapAdmin.js
+++ WebServer/public/js/mapAdmin.js
@@ -4,15 +4,33 @@
  *  License MIT
  */
 
+var capteur = L.AwesomeMarkers.icon({
+    icon:'connectdevelop',
+    prefix:'fa',
+    markerColor:'blue'
+});
 
-    
-    var map = L.map('mapAdmin').setView([45.19, 5.76], 14);
+var map = L.map('mapAdmin').setView([45.19, 5.76], 14);
 
-    var mapboxUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-        mapboxAttribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+var mapboxUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    mapboxAttribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 
-    L.tileLayer(mapboxUrl, {
-        attribution: mapboxAttribution,
-        maxZoom:18,
-    }).addTo(map);
+L.tileLayer(mapboxUrl, {
+    attribution: mapboxAttribution,
+    maxZoom:18,
+}).addTo(map);
+
+var marker;
+
+map.on('click', function(e) {
+    if(marker != null){
+        map.removeLayer(marker)
+    }
+    $("#latitude").val(e.latlng.lat);
+    $("#longitude").val(e.latlng.lng);
+    marker = new L.marker([e.latlng.lat, e.latlng.lng], {icon:capteur})
+    map.addLayer(marker);
+
+});
+
 
