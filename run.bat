ae74cfbf27e789ceb5add349c92d165ad37ff5d7
jejeLaVedette <PolytechRICM@192.168.1.81>
2015-03-11 11:46:37 +0100

    14504-6 lancement du site web depuis le script

--- run.bat
+++ run.bat
@@ -5,8 +5,12 @@ cd WebServer\initDB
 echo "init BDD :"
 initDB.bat
 echo "BDD init DONE!"
+
+start http://localhost:9999/client
+
 cd ..\
 echo "lancement serveur.js ..."
 start node serveur.js
 echo "serveur.js DONE!"
+
 )
